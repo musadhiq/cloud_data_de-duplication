a41948f0c5c0961fd8ad61d0f48e37722108eb58
musadhiq <musadhiqmp@gmail.com>
2022-09-19 23:17:44 +0530

    admin dashboard added

--- src/static/javascript/app.js
+++ src/static/javascript/app.js
@@ -1,17 +1,13 @@
-window.addEventListener("load", () => {
-  const Password = document.getElementById("password");
+const Password = document.getElementById("password");
 
-  const Eye = document.getElementById("eye");
+const Eye = document.getElementById("eye");
 
-  console.log(Password, Eye);
-
-  Eye.addEventListener("click", () => {
-    if (Password.type === "password") {
-      Password.type = "text";
-      Eye.classList.add("focused");
-    } else {
-      Password.type = "password";
-      Eye.classList.remove("focused");
-    }
-  });
+Eye.addEventListener("click", () => {
+  if (Password.type === "password") {
+    Password.type = "text";
+    Eye.classList.add("focused");
+  } else {
+    Password.type = "password";
+    Eye.classList.remove("focused");
+  }
 });
