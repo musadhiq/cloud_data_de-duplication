9438c57825b789294c56abc25d10ef11bfe5ecc6
musadhiq <musadhiqmp@gmail.com>
2022-10-09 13:55:30 +0530

    leader functions added -> profile ,manage works

--- src/app.py
+++ src/app.py
@@ -89,23 +89,6 @@ def edit_leader():
     return render_template("/admin/edit_leader.html", data=res)
 
 
-@app.route('/leaders/update', methods=['post'])
-def update_leader():
-    Lid = request.form['lid']
-    Fname = request.form['firstname']
-    Lname = request.form['lastname']
-    Gender = request.form['gender']
-    Place = request.form['place']
-    Pin = request.form['pin']
-    Post = request.form['post']
-    Email = request.form['email']
-    Phone = request.form['phone']
-    qry = "update leader set Fname=%s,Lname=%s,Gender=%s,Place=%s,Pin=%s,Post=%s,Email=%s,Phone=%s where id=%s"
-    val = (Fname, Lname, Gender, Place, Pin, Post, Email, Phone, Lid)
-    iud(qry, val)
-    return '''<script>alert("success");window.location="/manage_leaders"</script>'''
-
-
 @app.route('/leaders/delete')
 def delete_leader():
     id = request.args.get('id')
@@ -142,10 +125,14 @@ def delete_work():
 def assign_works():
     works = selectall("SELECT * FROM `works`")
     leaders = selectall("SELECT lid,Fname,Lname FROM `leader`")
-    assigned = selectall("SELECT * FROM `assign_to_leader`")
+    assign = selectall("SELECT * FROM `assign_to_leader`")
     filtered_works = list(filter(lambda x: x['workid'] not in [
-        y['wid'] for y in assigned], works))
-    return render_template("/admin/assign_work.html", works=filtered_works, leaders=leaders)
+        y['wid'] for y in assign], works))
+
+    assignedstatus = selectall(
+        "SELECT leader.lid, leader.fname, leader.lname ,works.workid, works.workname, works.status from leader JOIN assign_to_leader on assign_to_leader.lid = leader.lid JOIN works ON assign_to_leader.wid = works.workid")
+
+    return render_template("/admin/assign_work.html", works=filtered_works, leaders=leaders, assigned=assignedstatus)
 
 
 @app.route("/admin/assign_works1", methods=['post'])
@@ -175,24 +162,118 @@ def leader():
     return render_template("/leader/leader_home.html")
 
 
-@app.route('/leader_works')
-def leader_works():
-    return render_template("/works.html")
+@app.route('/leader/edit_profile')
+def edit_profile():
+    id = session['lid']
+    print(id)
+    qry = "SELECT * FROM `leader` WHERE `lid`=%s"
+    val = (id)
+    res = selectone(qry, val)
+    return render_template("/leader/edit_profile.html", data=res)
 
 
-@app.route('/manage_members')
+@app.route('/leader/update', methods=['post'])
+def update_leader():
+    Lid = request.form['lid']
+    Fname = request.form['firstname']
+    Lname = request.form['lastname']
+    Gender = request.form['gender']
+    Place = request.form['place']
+    Pin = request.form['pin']
+    Post = request.form['post']
+    Email = request.form['email']
+    Phone = request.form['phone']
+    qry = "update leader set Fname=%s,Lname=%s,Gender=%s,Place=%s,Pin=%s,Post=%s,Email=%s,Phone=%s where id=%s"
+    val = (Fname, Lname, Gender, Place, Pin, Post, Email, Phone, Lid)
+    iud(qry, val)
+    return '''<script>alert("success");window.location="/leader"</script>'''
+
+
+@app.route('/leader/manage_members')
 def manage_members():
-    return render_template("/manage_members.html")
+    data = selectall("SELECT lid,Fname,Lname,Email FROM `member`")
+    return render_template("/leader/manage_members.html", data=data)
 
 
-@app.route('/add_member')
+@app.route('/leader/add_member')
 def add_member():
     return render_template("/leader/add_member.html")
 
 
-@app.route('/edit_member')
+@app.route('/leader/add_member1', methods=['post'])
+def add_member1():
+    Fname = request.form['firstname']
+    Lname = request.form['lastname']
+    Gender = request.form['gender']
+    Place = request.form['place']
+    Pin = request.form['pin']
+    Post = request.form['post']
+    Email = request.form['email']
+    Phone = request.form['phone']
+    Username = request.form['username']
+    password = request.form['password']
+    qry = "insert into login values(Null,%s,%s,'member')"
+    val = (Username, password)
+    id = iud(qry, val)
+    qry1 = "insert  into member values (Null,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    val1 = (str(id), Fname, Lname, Gender, Place, Pin, Post, Email, Phone)
+    iud(qry1, val1)
+    return '''<script>alert("added");window.location="/leader/manage_members"</script>'''
+
+
+@app.route('/leader/edit_member')
 def edit_member():
-    return render_template("/leader/edit_member.html")
+    id = request.args.get('id')
+    session['id'] = id
+    qry = "SELECT * FROM `member` WHERE `lid`=%s"
+    val = (id)
+    res = selectone(qry, val)
+    print(res)
+    return render_template("/leader/edit_member.html", data=res)
+
+
+@app.route('/leader/edit_member1', methods=['post'])
+def edit_member1():
+    Lid = request.form['lid']
+    Fname = request.form['firstname']
+    Lname = request.form['lastname']
+    Gender = request.form['gender']
+    Place = request.form['place']
+    Pin = request.form['pin']
+    Post = request.form['post']
+    Email = request.form['email']
+    Phone = request.form['phone']
+    qry = "update member set fname=%s,lname=%s,gender=%s,place=%s,pin=%s,post=%s,email=%s,phone=%s where lid=%s"
+    val = (Fname, Lname, Gender, Place, Pin, Post, Email, Phone, Lid)
+    print(Lid)
+    iud(qry, val)
+    return '''<script>alert("success");window.location="/leader/manage_members"</script>'''
+
+
+@app.route('/leader/assign_works')
+def leader_works():
+    id = session['lid']
+    val = (id)
+    qry = "SELECT * FROM works WHERE `workid` IN (SELECT wid FROM `assign_to_leader` WHERE `lid`=%s)"
+    works = selectall2(qry, val)
+    members = selectall("SELECT * FROM `member`")
+    assign = selectall("SELECT * FROM `assign_to_member`")
+    filtered_works = list(filter(lambda x: x['workid'] not in [
+        y['wid'] for y in assign], works))
+    assignedstatus = selectall(
+        "SELECT member.lid, member.fname, member.lname ,works.workid, works.workname, works.status from member JOIN assign_to_member on assign_to_member.lid = member.lid JOIN works ON assign_to_member.wid = works.workid")
+
+    return render_template("/leader/assign_works.html", works=filtered_works, members=members, assigned=assignedstatus)
+
+
+@app.route('/leader/assign_works1', methods=['post'])
+def assign_to_mem():
+    workid = request.form['workid']
+    memberid = request.form['memberid']
+    qry = "insert into assign_to_member values(Null,%s,%s,%s,'pending')"
+    val = (memberid, workid, date)
+    iud(qry, val)
+    return '''<script>alert("assigned");window.location="/leader"</script>'''
 
 
 @app.route('/feedback')
