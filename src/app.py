1a5cee5e6d45a79d6c27d34009502f2c2f5b9109
musadhiq <musadhiqmp@gmail.com>
2022-10-17 23:04:27 +0530

    socket notification added

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
+from functools import wraps
 from flask import *
-
+from flask_socketio import SocketIO, send
 from src.dbconnection import *
 from src.templates import *
 from datetime import datetime
@@ -12,42 +13,105 @@ from src.member.member import member
 app = Flask(__name__)
 
 
-app.register_blueprint(admin, url_prefix='/admin')
-app.register_blueprint(leader, url_prefix='/leader')
-app.register_blueprint(member, url_prefix='/member')
+app.register_blueprint(admin, url_prefix="/admin")
+app.register_blueprint(leader, url_prefix="/leader")
+app.register_blueprint(member, url_prefix="/member")
 
 app.secret_key = "anystringhere"
+
+socketio = SocketIO(app)
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "lid" not in session:
+            return redirect("/")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@socketio.on("message")
+def handle_notification(msg):
+    print("received Notification: " + msg)
+    send(msg, broadcast=True)
+    qry = "insert into notification values (null, %s, %s)"
+    val = (msg, datetime.now())
+    iud(qry, val)
+
+
+@app.route("/chat")
+@login_required
+def chat():
+    return render_template("sock.html")
+
+
+def messageReceived(methods=["GET", "POST"]):
+    print("message was received!!!")
+
+
+@socketio.on("complaint")
+def handle_complaint(data):
+    print("received complaint: " + data)
+    socketio.emit("complaint", data, callback=messageReceived)
+
+
+@app.route("/test")
+def sockettest():
+    return render_template("socket.html")
+
 
 date = datetime.today()
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('login.html')
+    return render_template("login.html")
 
 
-@app.route('/login', methods=['post', 'get'])
+def set_session(id, username, email):
+    session["user"] = session["user"] = {
+        "id": id,
+        "username": username,
+        "email": email,
+    }
+
+
+@app.route("/login", methods=["post", "get"])
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form["username"]
+    password = request.form["password"]
     qry = "SELECT * FROM `login` WHERE `uname`=%s AND `password`=%s"
     val = (username, password)
     res = selectone(qry, val)
     if res is None:
         flash("incorrect username or password.")
-        return '''<script>window.location="/";</script>'''
-    elif res['type'] == 'admin':
-        id = res['id']
-        session['lid'] = id
-        return '''<script>alert("welcome to admin");window.location="/admin"</script>'''
-    elif res['type'] == 'leader':
-        id = res['id']
-        session['lid'] = id
-        return '''<script>alert("welcome to leader");window.location="/leader"</script>'''
-    elif res['type'] == 'member':
-        id = res['id']
-        session['lid'] = id
-        return '''<script>alert("welcome to member");window.location="/member"</script>'''
+        return """<script>window.location="/";</script>"""
+    elif res["type"] == "admin":
+        id = res["id"]
+        userdata = selectall("select * from admin where id=%s", (id))
+        print(userdata)
+        session["user"] = id
+        return """<script>alert("welcome to admin");window.location="/admin"</script>"""
+    elif res["type"] == "leader":
+        id = res["id"]
+        userdata = selectall2("select * from leader where lid=%s", (id))
+        set_session(userdata["lid"], res["uname"], userdata["email"])
+        return (
+            """<script>alert("welcome to leader");window.location="/leader"</script>"""
+        )
+    elif res["type"] == "member":
+        id = res["id"]
+        session["lid"] = id
+        return (
+            """<script>alert("welcome to member");window.location="/member"</script>"""
+        )
     else:
         flash("incorrect username or password.")
-        return '''<script>window.location="/";</script>'''
+        return """<script>window.location="/";</script>"""
+
+
+if __name__ == "__main__":
+    socketio.run(app)
--- src/member/member.py
+++ src/member/member.py
@@ -2,61 +2,64 @@ from flask import *
 from src.dbconnection import *
 from datetime import datetime
 
-member = Blueprint('member', __name__, template_folder='templates')
+member = Blueprint("member", __name__, template_folder="templates")
 
 
 @member.route("/")
 def member_home():
-    return render_template("member_home.html")
+    notification_data = selectall(
+        "SELECT * FROM `notification` order by notificationid desc"
+    )
+    return render_template("member_home.html", notificationdata=notification_data)
 
 
-@member.route('/manage_profile')
+@member.route("/manage_profile")
 def member_profile():
-    id = session['lid']
+    id = session["lid"]
     qry = "SELECT * FROM `member` WHERE `lid`=%s"
-    val = (id)
+    val = id
     res = selectone(qry, val)
     return render_template("m_editprofile.html", data=res)
 
 
-@member.route('/update', methods=['post'])
+@member.route("/update", methods=["post"])
 def update_member():
-    Lid = request.form['lid']
-    Fname = request.form['firstname']
-    Lname = request.form['lastname']
-    Gender = request.form['gender']
-    Place = request.form['place']
-    Pin = request.form['pin']
-    Post = request.form['post']
-    Email = request.form['email']
-    Phone = request.form['phone']
+    Lid = request.form["lid"]
+    Fname = request.form["firstname"]
+    Lname = request.form["lastname"]
+    Gender = request.form["gender"]
+    Place = request.form["place"]
+    Pin = request.form["pin"]
+    Post = request.form["post"]
+    Email = request.form["email"]
+    Phone = request.form["phone"]
     qry = "update member set fname=%s,lname=%s,gender=%s,place=%s,pin=%s,post=%s,email=%s,phone=%s where lid=%s"
     val = (Fname, Lname, Gender, Place, Pin, Post, Email, Phone, Lid)
     print(Lid)
     iud(qry, val)
-    return '''<script>alert("success");window.location="/member"</script>'''
+    return """<script>alert("success");window.location="/member"</script>"""
 
 
-@member.route('/works')
+@member.route("/works")
 def member_work():
     return render_template("member_work.html")
 
 
-@member.route('/notifications')
+@member.route("/notifications")
 def notifications():
     return render_template("notification.html")
 
 
-@member.route('/send_feedback')
+@member.route("/send_feedback")
 def send_feedback():
     return render_template("send_feedback.html")
 
 
-@member.route('/ask_doubt')
+@member.route("/ask_doubt")
 def ask_doubt():
     return render_template("ask_doubt.html")
 
 
-@member.route('/post_complaint')
+@member.route("/post_complaint")
 def post_complaint():
     return render_template("post_complaint.html")
